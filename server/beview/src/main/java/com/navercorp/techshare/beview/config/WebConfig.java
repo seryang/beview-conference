3beb92af9d5a797c6562c157d5e718b6432694f3
Naver <Naver@Naverui-MacBook-Pro-173.local>
2017-01-10 14:03:43 +0900

    [server] Spring Boot Init (JDBC, DBCP, Jackson, MySQL, Lombok)

---
+++ server/beview/src/main/java/com/navercorp/techshare/beview/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.navercorp.techshare.beview.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Naver on 2017. 1. 10..
+ */
+@EnableWebMvc
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON);
+
+        configurer.favorPathExtension(false).
+                favorParameter(true).
+                parameterName("format").
+                ignoreAcceptHeader(true).
+                useJaf(false).
+                defaultContentType(MediaType.APPLICATION_JSON).
+                mediaType("xml", MediaType.APPLICATION_XML).
+                mediaType("json", MediaType.APPLICATION_JSON);
+    }
+}
