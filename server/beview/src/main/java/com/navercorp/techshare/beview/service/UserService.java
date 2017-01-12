ac4d5decccad3a43662ec0d4dca35e09299991f8
Naver <Naver@Naverui-MacBook-Pro-173.local>
2017-01-12 19:59:29 +0900

    [server] Access-Control-Expose-Header 추가

--- server/beview/src/main/java/com/navercorp/techshare/beview/service/UserService.java
+++ server/beview/src/main/java/com/navercorp/techshare/beview/service/UserService.java
@@ -1,10 +1,13 @@
 package com.navercorp.techshare.beview.service;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.sun.net.httpserver.HttpContext;
 
 import com.navercorp.techshare.beview.exception.AuthorizationException;
 import com.navercorp.techshare.beview.model.User;
@@ -19,6 +22,9 @@ public class UserService {
 
 	@Autowired
 	private HttpServletResponse response;
+
+	@Autowired
+	private HttpServletRequest request;
 
 	@Autowired
 	private UserDao userDao;
@@ -42,9 +48,11 @@ public class UserService {
 		Cookie cookie1 = new Cookie("password", existUser.getPassword());
 
 		final int MAX_AGE = 24 * 60 * 60;
-
+		response.setHeader("Access-Control-Expose-Headers", "Set-Cookie");
 		cookie.setMaxAge(MAX_AGE);
+		cookie.setPath("/");
 		cookie1.setMaxAge(MAX_AGE);
+		cookie1.setPath("/");
 		response.addCookie(cookie);
 		response.addCookie(cookie1);
 
