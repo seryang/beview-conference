9d1b8ddd9e1a3ceaaeec9a88de11abf22e2ef4af
박성현[FE플랫폼] <ian.park@navercorp.com>
2017-01-12 10:49:52 +0900

    [FE] remove http module Promise wrapping update ajax request url update ajax request timeout

--- fe/src/js/http.js
+++ fe/src/js/http.js
@@ -2,13 +2,14 @@ var http = (function () {
   'use strict';
 
   var publicApi = {};
-  var BASE_URL = 'http://localhost:3000' + '/api';
+  // var BASE_URL = 'http://10.83.24.172:8080' + '/api';
+  var BASE_URL = '/api';
   var methods = ['get', 'post', 'put', 'delete'];
 
   // url convert 에서 {dynamic string} 을 판별할 정규식
   var reParam = /(\{(\w+)\})/g;
 
-  //  url path 내 {dynamic string} 을 params 내에서 key 참조를 통해 찾아서 바꿔준다.
+  // url path 내 {dynamic string} 을 params 내에서 key 참조를 통해 찾아서 바꿔준다.
   function convertUrl (url, params) {
     var converted = url;
     var matches;
@@ -24,20 +25,19 @@ var http = (function () {
 
   methods.forEach(function (method) {
     publicApi[method] = function (options) {
-      return new Promise(function (resolve, reject) {
+      if (options.params) {
+        options.url = convertUrl(options.url, options.params);
+      }
 
-        if (options.params) {
-          options.url = convertUrl(options.url, options.params);
-        }
-
-        Object.assign(options, {
-          method: method,
-          url: BASE_URL + options.url,
-          dataType: 'json'
-        });
-
-        $.ajax(options).then(resolve, reject);
+      Object.assign(options, {
+        method: method,
+        url: BASE_URL + options.url,
+        contentType: 'application/json',
+        dataType: 'json',
+        timeout: 5000
       });
+
+      return $.ajax(options);
     };
   });
 
