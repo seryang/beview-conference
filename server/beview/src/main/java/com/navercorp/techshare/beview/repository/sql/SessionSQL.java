8a0465e424d850859e26a9dcb70c987cff129b7e
Naver <Naver@Naverui-MacBook-Pro-173.local>
2017-01-16 16:48:11 +0900

    [server] 세션 (CRUD) API

--- server/beview/src/main/java/com/navercorp/techshare/beview/repository/sql/SessionSQL.java
+++ server/beview/src/main/java/com/navercorp/techshare/beview/repository/sql/SessionSQL.java
@@ -1,9 +1,13 @@
 package com.navercorp.techshare.beview.repository.sql;
 
-
 /**
  * Created by Naver on 2017. 1. 12..
  */
 public class SessionSQL {
 	public static final String SESSION_INSERT = "INSERT INTO session VALUES (null, ?, ?, ?, ?, ?, ?, ?)";
+	public static final String SESSION_CHECK = "SELECT * FROM session INNER JOIN track on session.track_idx = ? where session.name = ?";
+	public static final String SESSION_SELECT = "SELECT * FROM session WHERE idx = ?";
+	public static final String SESSION_UPDATE = "UPDATE session SET name = ?, description = ?, startDate = ?, endDate = ?, file = ?, track_idx = ?, speaker_idx = ? where idx = ?";
+	public static final String SESSION_SELECT_ALL = "SELECT * FROM session";
+	public static final String SESSION_DELETE = "DELETE from session WHERE idx = ?";
 }
