b453451f60040c4de70e947e15f94afc0626a442
Naver <Naver@Naverui-MacBook-Pro-173.local>
2017-01-17 18:19:52 +0900

    [server] 찜하기 기능 ( 찜 , 찜삭제)

--- server/beview/src/main/java/com/navercorp/techshare/beview/controller/FavoriteController.java
+++ server/beview/src/main/java/com/navercorp/techshare/beview/controller/FavoriteController.java
@@ -1,14 +1,18 @@
 package com.navercorp.techshare.beview.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.navercorp.techshare.beview.annotation.Auth;
+import com.navercorp.techshare.beview.exception.AuthorizationException;
+import com.navercorp.techshare.beview.exception.Error;
 import com.navercorp.techshare.beview.model.response.AjaxResponse;
+import com.navercorp.techshare.beview.service.FavoriteService;
 
 /**
  * Created by Naver on 2017. 1. 16..
@@ -17,24 +21,38 @@ import com.navercorp.techshare.beview.model.response.AjaxResponse;
 @RequestMapping("/api/favorites")
 public class FavoriteController {
 
-	// 찜하기
+	@Autowired
+	private FavoriteService favoriteService;
+
+	/**
+	 * [찜하기]
+	 *
+	 * @param sessionIdx
+	 * @param userId
+	 * @return AjaxResponse
+	 */
 	@Auth
 	@PostMapping("{sessionIdx}")
-	public AjaxResponse favoriteSession(@PathVariable Integer sessionIdx){
-
+	public AjaxResponse createFavorite(@PathVariable Integer sessionIdx, @CookieValue(value = "id", defaultValue = "0") String userId){
+		if("0".equals(userId)){
+			throw new AuthorizationException(Error.AUTHORIZED_FAIL);
+		}
+		return favoriteService.createFavorite(sessionIdx, userId);
 	}
 
-	// 찜하기 삭제
+	/**
+	 * [찜하기 삭제]
+	 *
+	 * @param sessionIdx
+	 * @param userId
+	 * @return
+	 */
 	@Auth
 	@DeleteMapping("/{sessionIdx}")
-	public AjaxResponse favoriteSession(@PathVariable Integer sessionIdx){
-
-	}
-
-	// 찜하기 전체 리스트
-	@Auth
-	@GetMapping()
-	public AjaxResponse favoriteSession(){
-
+	public AjaxResponse deleteFavorite(@PathVariable Integer sessionIdx, @CookieValue(value = "id", defaultValue = "0") String userId){
+		if("0".equals(userId)){
+			throw new AuthorizationException(Error.AUTHORIZED_FAIL);
+		}
+		return favoriteService.deleteFavorite(sessionIdx, userId);
 	}
 }
