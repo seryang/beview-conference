39eee5bd64f82ed2e0526ac763efdf0f31f82b41
Naver <Naver@Naverui-MacBook-Pro-173.local>
2017-01-18 00:04:09 +0900

    [server] Session SQL 수정

--- server/beview/src/main/java/com/navercorp/techshare/beview/repository/sql/SessionSQL.java
+++ server/beview/src/main/java/com/navercorp/techshare/beview/repository/sql/SessionSQL.java
@@ -6,9 +6,9 @@ package com.navercorp.techshare.beview.repository.sql;
 public class SessionSQL {
 	public static final String SESSION_INSERT = "INSERT INTO session VALUES (null, ?, ?, ?, ?, ?, ?, ?)";
 	public static final String SESSION_CHECK = "SELECT * FROM session INNER JOIN track on session.track_idx = ? where session.name = ?";
-	public static final String SESSION_CHECK_BEFORE_UPDATE = "SELECT * FROM session INNER JOIN track on session.track_idx = ? where session.name = ? and idx != ?";
+	public static final String SESSION_CHECK_BEFORE_UPDATE = "SELECT * FROM session INNER JOIN track on session.track_idx = ? where session.name = ? and session.idx != ?";
 	public static final String SESSION_SELECT = "SELECT * FROM session WHERE idx = ?";
-	public static final String SESSION_UPDATE = "UPDATE session SET name = ?, description = ?, startDate = ?, endDate = ?, file = ?, track_idx = ?, speaker_idx = ? where idx = ?";
+	public static final String SESSION_UPDATE = "UPDATE session SET name = ?, description = ?, startTime = ?, endTime = ?, file = ?, track_idx = ?, speaker_idx = ? where session.idx = ?";
 	public static final String SESSION_SELECT_ALL = "SELECT * FROM session";
 	public static final String SESSION_DELETE = "DELETE from session WHERE idx = ?";
 }
