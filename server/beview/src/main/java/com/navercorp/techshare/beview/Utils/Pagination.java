407a3de22ae1c408adccfc500aa4bf86f57d01b9
seungho.choi <seungho.choi@navercorp.com>
2017-01-18 17:08:21 +0900

    [SERVER] Pagination class 정의

---
+++ server/beview/src/main/java/com/navercorp/techshare/beview/Utils/Pagination.java
@@ -0,0 +1,14 @@
+package com.navercorp.techshare.beview.Utils;
+
+/**
+ * Created by seungdols on 2017. 1. 18..
+ */
+public class Pagination {
+	static private final Integer PAGESIZE = 3;
+	static public Integer getStart(Integer page) {
+		return (page-1) * PAGESIZE;
+	}
+	static public Integer getEnd() {
+		return PAGESIZE;
+	}
+}
