b368138465c5a417ea699436519e291e045d90c7
seungho.choi <seungho.choi@navercorp.com>
2017-01-19 13:45:58 +0900

    [SERVER] paging size 10으로 조정

--- server/beview/src/main/java/com/navercorp/techshare/beview/Utils/Pagination.java
+++ server/beview/src/main/java/com/navercorp/techshare/beview/Utils/Pagination.java
@@ -4,7 +4,7 @@ package com.navercorp.techshare.beview.Utils;
  * Created by seungdols on 2017. 1. 18..
  */
 public class Pagination {
-	static private final Integer PAGESIZE = 3;
+	static private final Integer PAGESIZE = 10;
 	static public Integer getStart(Integer page) {
 		return (page-1) * PAGESIZE;
 	}
